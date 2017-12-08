13806f6081b2eef65e0cc04174288c57302ad7bf
wowonrails <wowaonrails@gmail.com>
2017-12-08 12:45:48 +0300

    Fixing rubocop issue

--- lib/csv_file_manager/file_manager.rb
+++ lib/csv_file_manager/file_manager.rb
@@ -3,7 +3,7 @@ require "csv_file_manager/file_path"
 module CsvFileManager
   class FileManager
     class << self
-      def create(class_name, headers = [])
+      def create(class_name, _headers = [])
         CSV.open(FilePath.retrieve(class_name), "w+")
       end
 
